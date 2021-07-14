b7e6da6618f469809202b27c51adc027b56eb3ad
심재우 <jw_shim@naver.com>
2021-07-14 23:42:30 +0900

    그리디

---
+++ BOJ/Main_BOJ_G5_11000_강의실배정.java
@@ -0,0 +1,63 @@
+package BOJ;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main_BOJ_G5_11000_강의실배정 {
+
+	static class Meeting{
+		int start;
+		int end;
+		public Meeting(int start, int end) {
+			super();
+			this.start = start;
+			this.end = end;
+		}
+		@Override
+		public String toString() {
+			return "Meeting [start=" + start + ", end=" + end + "]";
+		}
+		
+		
+	}
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		
+		int N = scann.nextInt();
+		
+		Meeting[] meeting = new Meeting[N];
+		
+		for (int i = 0; i < N; i++) {
+			int s = scann.nextInt();
+			int e = scann.nextInt();
+			
+			meeting[i] = new Meeting(s,e);
+		}
+		
+		Arrays.sort(meeting, new Comparator<Meeting>(){
+
+			@Override
+			public int compare(Meeting o1, Meeting o2) {
+				if (o1.start==o2.start) {
+					return o1.end - o2.end;
+				}
+				return o1.start-o2.start;
+			}
+			
+		});
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		pq.offer(meeting[0].end);
+		
+		for (int i = 1; i < N; i++) {
+			if (pq.peek() <= meeting[i].start) {
+				pq.poll();
+			}
+			pq.offer(meeting[i].end);
+		}
+		System.out.println(pq.size());
+	}
+
+}
