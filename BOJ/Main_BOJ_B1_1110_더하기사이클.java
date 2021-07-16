d99afe9c5f9e28e81c6aa3afbd2f13817cc00bd1
심재우 <jw_shim@naver.com>
2021-07-17 00:45:00 +0900

    .

---
+++ BOJ/Main_BOJ_B1_1110_더하기사이클.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Main_BOJ_B1_1110_더하기사이클 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int N = scan.nextInt();
+		int next = ((N%10)*10) + (((N/10)+(N%10))%10);
+		int cnt = 1;
+		while(next!=N) {
+			next = ((next%10)*10) + (((next/10)+(next%10))%10);
+			cnt++;
+		}
+		System.out.println(cnt);
+		
+	}
+
+}
