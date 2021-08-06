48c94771a7e13e9ddd36be02cf3ec3239ce4f52e
심재우 <jw_shim@naver.com>
2021-08-06 21:24:32 +0900

    .

---
+++ BOJ/Main_BOJ_S3_15650_N과M2.java
@@ -0,0 +1,36 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Main_BOJ_S3_15650_N과M2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		boolean[] v = new boolean[N];
+		int[] num = new int[M];
+		perm(0, 0, N, M, v, num);
+	}
+	static StringBuilder sb = new StringBuilder();
+	private static void perm(int start, int cnt, int N, int M, boolean[] v, int[] num) {
+		if (cnt == M) {
+			for (int i = 0; i < M; i++) {
+				System.out.print(num[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			if (v[i]) {
+				continue;
+			}
+			v[i]=true;
+			num[cnt] = i+1;
+			perm(i+1,cnt+1, N, M, v, num);
+			v[i]=false;
+		}
+			
+	}
+
+}
