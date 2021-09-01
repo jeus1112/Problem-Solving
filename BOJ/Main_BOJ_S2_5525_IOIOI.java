485a5f1b54c9a7e78f4d53d449ec57458031b0f8
심재우 <jw_shim@naver.com>
2021-09-01 23:45:21 +0900

    dp

---
+++ BOJ/Main_BOJ_S2_5525_IOIOI.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_BOJ_S2_5525_IOIOI {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        char[] arr = br.readLine().toCharArray();
+
+        int ans = 0;
+        int[] dist = new int[M];
+
+        for (int i = 1; i < M - 1; i++) {
+            if (arr[i] == 'O' && arr[i + 1] == 'I') {
+                dist[i+1]=dist[i-1]+1;
+                if (dist[i + 1] >= N && arr[i - 2 * N + 1] == 'I') {
+                    ans++;
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+}
