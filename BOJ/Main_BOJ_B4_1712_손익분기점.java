12ed92c19d909890b3426c133b57f23f056f0b60
심재우 <jw_shim@naver.com>
2021-09-10 21:36:45 +0900

    BOJ 1712

---
+++ BOJ/Main_BOJ_B4_1712_손익분기점.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_BOJ_B4_1712_손익분기점 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int BC = B-C;
+        if (BC >= 0) {
+            System.out.println("-1");
+        }else{
+            System.out.println((A/(-BC))+1);    //BC가 0이면 ArithmeticException 발생
+        }
+    }
+}
