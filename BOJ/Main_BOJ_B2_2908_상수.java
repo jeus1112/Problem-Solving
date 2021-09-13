3ed87c3263fcf7f0fe7e304c251639e8701843fe
심재우 <jw_shim@naver.com>
2021-09-13 22:46:33 +0900

    BOJ 2908

---
+++ BOJ/Main_BOJ_B2_2908_상수.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_BOJ_B2_2908_상수 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        sb.append(st.nextToken());
+        int a = Integer.parseInt(sb.reverse().toString());
+        sb = new StringBuffer();
+        sb.append(st.nextToken());
+        int b = Integer.parseInt(sb.reverse().toString());
+        if (a > b) {
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
