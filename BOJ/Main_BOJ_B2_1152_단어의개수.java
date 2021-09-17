852ab5c4be75b61b60342e77fa89373cd31eb69c
심재우 <jw_shim@naver.com>
2021-09-18 00:19:57 +0900

    BOJ 1152

---
+++ BOJ/Main_BOJ_B2_1152_단어의개수.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+import java.io.*;
+
+public class Main_BOJ_B2_1152_단어의개수 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        if (str.equals(" ")) {  // 공백 하나만 들어올 수도 있다.
+            System.out.println("0");
+            System.exit(0);
+        }
+        String[] ss = str.split(" ");
+        if (ss[0].equals("")) {
+            System.out.println(ss.length-1);
+        }
+        else{
+            System.out.println(ss.length);
+        }
+    }
+}
