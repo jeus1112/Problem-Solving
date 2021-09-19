767ccac35747ba5734c2cec3bc2eee30c34db212
심재우 <jw_shim@naver.com>
2021-09-19 22:35:20 +0900

    BOJ 8393

---
+++ BOJ/Main_BOJ_8393_합_B5.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Main_BOJ_8393_합_B5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n*(1+n)/2);
+    }
+}
