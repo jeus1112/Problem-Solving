d7ea965d92452e247f50a6b8ed3afeb0281add31
심재우 <jw_shim@naver.com>
2021-09-21 00:05:15 +0900

    BOJ 10171

---
+++ BOJ/Main_BOJ_10171_고양이_B5.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+public class Main_BOJ_10171_고양이_B5 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
