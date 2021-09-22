7f75e601f5a16e9f6d83ad5333024ad4690a805f
심재우 <jw_shim@naver.com>
2021-09-22 17:14:51 +0900

    BOJ 10172

---
+++ BOJ/Main_BOJ_10172_개_B5.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+public class Main_BOJ_10172_개_B5 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
