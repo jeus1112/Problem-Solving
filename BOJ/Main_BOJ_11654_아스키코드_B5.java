97e556a5b3609215c24ce32dcd390763f9921cd0
심재우 <jw_shim@naver.com>
2021-09-24 22:30:25 +0900

    BOJ 11654

--- BOJ/Main_BOJ_11654_아스키코드_B5.java
+++ BOJ/Main_BOJ_11654_아스키코드_B5.java
@@ -2,7 +2,7 @@ package BOJ;
 
 import java.io.*;
 
-public class Main_BOJ_B5_11654_아스키코드 {
+public class Main_BOJ_11654_아스키코드_B5 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
