c4f59c82f070eb4008a74a1faaa4319f61647556
심재우 <jw_shim@naver.com>
2021-09-24 22:31:49 +0900

    BOJ 16397

--- BOJ/Main_BOJ_16397_탈출_G4.java
+++ BOJ/Main_BOJ_16397_탈출_G4.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main_BOJ_G4_16397_탈출 {
+public class Main_BOJ_16397_탈출_G4 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
