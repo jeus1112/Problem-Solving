9c3e7eb9b047a7d551f485d26169505f036347c3
심재우 <jw_shim@naver.com>
2021-09-26 12:43:48 +0900

    BOJ 1817

--- BOJ/Main_BOJ_1817_짐챙기는숌_S4.java
+++ BOJ/Main_BOJ_1817_짐챙기는숌_S4.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_BOJ_S4_1817_짐챙기는숌 {
+public class Main_BOJ_1817_짐챙기는숌_S4 {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
