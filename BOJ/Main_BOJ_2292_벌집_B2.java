71a38dd6dd8b4cfb1fdc61f7bef6bf8772e2153d
심재우 <jw_shim@naver.com>
2021-09-26 12:44:26 +0900

    BOJ 2292

--- BOJ/Main_BOJ_2292_벌집_B2.java
+++ BOJ/Main_BOJ_2292_벌집_B2.java
@@ -2,7 +2,7 @@ package BOJ;
 
 import java.io.*;
 
-public class Main_BOJ_2202_벌집_B2 {
+public class Main_BOJ_2292_벌집_B2 {
     public static void main(String[] args) throws Exception{
         // 1  2~7  8~19  20~37  38~61
         // 1   6    12     18   24
