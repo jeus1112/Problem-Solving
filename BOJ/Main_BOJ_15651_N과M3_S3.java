e710c47e240d0f0af079d7539867f052245ed431
심재우 <jw_shim@naver.com>
2021-10-04 00:44:50 +0900

    BOJ 15651

--- BOJ/Main_BOJ_15651_N과M3_S3.java
+++ BOJ/Main_BOJ_15651_N과M3_S3.java
@@ -2,7 +2,7 @@ package BOJ;
 
 import java.util.Scanner;
 
-public class Main_BOJ_S3_15651_N과M3 {
+public class Main_BOJ_15651_N과M3_S3 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
