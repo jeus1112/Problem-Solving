cb9e2b018614eeda82505d32f08436e15eade789
심재우 <jw_shim@naver.com>
2021-10-04 00:21:18 +0900

    BOJ 13549

--- BOJ/Main_BOJ_13549_숨바꼭질3_G5.java
+++ BOJ/Main_BOJ_13549_숨바꼭질3_G5.java
@@ -3,8 +3,14 @@ package BOJ;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
-public class Main_BOJ_G5_13549_숨바꼭질3 {
+/**
+ * @Date :
+ * @Author : JAEWOO SHIM
+ *
+ * bfs
+ *
+ */
+public class Main_BOJ_13549_숨바꼭질3_G5 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
