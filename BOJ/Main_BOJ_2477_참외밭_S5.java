1a9f7c6c907f18db256290ceef91045f65272d74
심재우 <jw_shim@naver.com>
2021-10-04 00:23:08 +0900

    BOJ 2477

--- BOJ/Main_BOJ_2477_참외밭_S5.java
+++ BOJ/Main_BOJ_2477_참외밭_S5.java
@@ -1,8 +1,16 @@
 package BOJ;
 
 import java.util.Scanner;
-
-public class Main_BOJ_S5_2477_참외밭 {
+/**
+ * @Date :
+ * @Author : JAEWOO SHIM
+ *
+ * 무조건 변의 순서대로 반시계방향으로 진행하므로
+ * 가장 긴 가로와 세로의 변에서 각각 3칸씩 가면
+ * 잘려나가는 세로와 가로의 길이를 알 수 있다.
+ *
+ */
+public class Main_BOJ_2477_참외밭_S5 {
 
     public static void main(String[] args) {
 
@@ -20,13 +28,13 @@ public class Main_BOJ_S5_2477_참외밭 {
             x = sc.nextInt();
             cham[i] = sc.nextInt();
             if (x == 1 || x == 2) {
-                w = Math.max(w, cham[i]);
+                w = Math.max(w, cham[i]); //가로 최대 길이 찾기
                 if (w == cham[i]) {
                     wi = i;
                 }
             }
             if (x == 3 || x == 4) {
-                h = Math.max(h, cham[i]);
+                h = Math.max(h, cham[i]); //세로 최대 길이 찾기
                 if (h == cham[i]) {
                     hi = i;
                 }
@@ -35,7 +43,7 @@ public class Main_BOJ_S5_2477_참외밭 {
         int a = 0;
         int b = 0;
         if (wi + 3 >= 6) {
-            wi = wi + 3 - 6;
+            wi = wi + 3 - 6; // 초과한 인덱스 처리
             a = cham[wi];
         } else {
             a = cham[wi + 3];
