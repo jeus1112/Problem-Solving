162316bbb6bc10f883346a256bcbb9a77999a6e5
심재우 <jw_shim@naver.com>
2021-10-04 00:51:06 +0900

    BOJ 5525

--- BOJ/Main_BOJ_5525_IOIOI_S2.java
+++ BOJ/Main_BOJ_5525_IOIOI_S2.java
@@ -3,7 +3,7 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main_BOJ_S2_5525_IOIOI {
+public class Main_BOJ_5525_IOIOI_S2 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
