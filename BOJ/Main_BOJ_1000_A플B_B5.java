0a95946cc49cef8d1a13ec4ea300ad44577906c5
심재우 <jw_shim@naver.com>
2021-12-19 18:40:38 +0900

    BOJ 1000

---
+++ BOJ/Main_BOJ_1000_A플B_B5.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class Main_BOJ_1000_A플B_B5 {
+    public static void main(String[] args) {
+        Scanner scann=new Scanner(System.in);
+        int a=scann.nextInt();
+        int b=scann.nextInt();
+        System.out.println((a+b));
+    }
+}
