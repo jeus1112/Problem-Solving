598dfacfbb85af39a5092f0997cb656ee95bf914
심재우 <jw_shim@naver.com>
2021-12-19 18:42:03 +0900

    BOJ 10951

---
+++ BOJ/Main_BOJ_10951_A플B_4_B3.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.util.*;
+
+public class Main_BOJ_10951_A플B_4_B3 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
