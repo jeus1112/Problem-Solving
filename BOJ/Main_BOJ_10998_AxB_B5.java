7b02f1b47e15a02300bc4a0951880d167fa9302a
심재우 <jw_shim@naver.com>
2021-12-19 18:42:25 +0900

    BOJ 10998

---
+++ BOJ/Main_BOJ_10998_AxB_B5.java
@@ -0,0 +1,12 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_BOJ_10998_AxB_B5 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.println(Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken()));
+    }
+}
